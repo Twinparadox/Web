f4bdefc6d361369b51def65c59c2132087d09cd6
Wonwoo Nam <nww731@gmail.com>
2020-06-28 22:00:13 +0900

    Node.js Sequelize

---
+++ learn-sequelize/models/comment.js
@@ -0,0 +1,16 @@
+// Define Model
+moduel.exports = (sequelize, DataTypes) => {
+    return sequelize.define('comment', {
+        comment: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            allowNull: true,
+            defaultValue: sequelize.literal('now()'),
+        },
+    }, {
+        timestamps: false,
+    });
+};
